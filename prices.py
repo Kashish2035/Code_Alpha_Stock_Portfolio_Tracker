c7b8aab17eb6dd3637e7497b912e514955a71ab3
Kashish2035 <kashishneware20@gmail.com>
2025-12-11 12:22:33 +0530

    Create prices.py

---
+++ prices.py
@@ -0,0 +1,11 @@
+# prices.py
+# Hardcoded dictionary containing stock prices
+STOCK_PRICES = {
+    "AAPL": 180,
+    "TSLA": 250,
+    "GOOGL": 140,
+    "AMZN": 130,
+    "MSFT": 370,
+    "META": 290,
+    "NFLX": 400
+}
